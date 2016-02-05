5efaa0a871f74505369739f5abf36c8daa6f3eac
onionandpixel <onionandpixle@gmail.com>
2016-02-05 11:12:17 +0100

    test coverage with real coverable classes, improve coverage report to show class contents with coverage

---
+++ app/src/main/java/com/example/circletest/ExampleClass.java
@@ -0,0 +1,11 @@
+package com.example.circletest;
+
+/**
+ * Class to test unit test coverage report.
+ * Created by Ferenc_Wieszt on 2/5/2016.
+ */
+public class ExampleClass {
+    public boolean retunrTrue() {
+        return true;
+    }
+}
