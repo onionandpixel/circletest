2b9bde55447f82837992f605629e657fc898bae2
onionandpixel <onionandpixle@gmail.com>
2016-02-11 11:25:28 +0100

    add googledrive deployment

--- app/src/main/java/com/example/circletest/ExampleClass.java
+++ app/src/main/java/com/example/circletest/ExampleClass.java
@@ -2,7 +2,7 @@ package com.example.circletest;
 
 /**
  * Class to test unit test coverage report.
- * Created by Ferenc_Wieszt on 2/5/2016.
+ * Created by onionandpixel on 2/5/2016.
  */
 public class ExampleClass {
     public boolean retunrTrue() {
